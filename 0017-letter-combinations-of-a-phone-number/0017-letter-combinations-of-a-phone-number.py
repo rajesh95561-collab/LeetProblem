9bce1ad9a1da2749ee2bb697fdb790f2f68a4c52
Rajesh Kumar Sahoo <237406716+rajesh95561-collab@users.noreply.github.com>
2026-09-15 11:58:51 +0530

    Time: 0 ms (100%), Space: 19.4 MB (41.33%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,14 +1,16 @@
 class Solution:
     def backtrack(self,idx,temp,result,digits,my_dict):
         if idx == len(digits):
-            result.append(temp)
+            result.append("".join(temp))
             return
         for i in my_dict[digits[idx]]:
-            temp+=i
+            temp.append(i)
             self.backtrack(idx+1,temp,result,digits,my_dict)
-            temp = temp[:-1]
+            temp.pop()
     def letterCombinations(self, digits: str) -> List[str]:
+        if not digits:
+            return []
         my_dict = {"2":"abc","3":"def","4":"ghi","5":"jkl","6":"mno","7":"pqrs","8":"tuv","9":"wxyz"}
         result = []
-        self.backtrack(0,"",result,digits,my_dict)
+        self.backtrack(0,[],result,digits,my_dict)
         return result
